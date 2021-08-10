646541ac9efcb7175e66da48b2d3be550f9b6c34
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2021-08-10 14:57:20 +0200

    add configsMap/secrets

--- hw02_k8s/hw02-k8s-service.sh
+++ hw02_k8s/hw02-k8s-service.sh
@@ -2,10 +2,12 @@
 
 RELEASE_NAME="prod"
 PG_RELEASE_NAME="pg"
+TESTS_PATH="pg"
 CHART_DIR="k8s/charts/hw02-k8s-service"
+TESTS_FILE_PATH="Users_API.postman_collection.json"
 
 function usage {
-    echo "Usage: hw02-k8s-service {start|stop|installDB|dropDB}"
+    echo "Usage: hw02-k8s-service {start|stop|installDB|dropDB|test}"
     exit 1
 }
 
@@ -23,14 +25,24 @@ function dropDB {
 
 function startService {
     echo "Starting service...."
+    echo "Updating /etc/hosts...."
+    sudo sh -c 'echo "$(minikube ip) arch.homework" >> /etc/hosts'
+    echo "Updating /etc/hosts....Done"
     helm upgrade --install $RELEASE_NAME $CHART_DIR
     echo "Starting service....Done"
 }
 
 function stopService {
     echo "Stopping service...."
+    echo "Updating /etc/hosts...."
+    sudo sed -i '' '/arch\.homework$/d' /etc/hosts
+    echo "Updating /etc/hosts....Done"
     helm uninstall $RELEASE_NAME
     echo "Stopping service....Done"
+}
+
+function runTests {
+  newman run $TESTS_FILE_PATH
 }
 
 case $1 in
@@ -38,5 +50,6 @@ case $1 in
   dropDB) dropDB ;;
   start) startService ;;
   stop) stopService ;;
+  test) runTests ;;
   *) usage ;;
 esac
